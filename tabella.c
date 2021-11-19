47baea440ee20fa41b75f803a6da9c2ff05775fb
mrKpax <94631872+mrKpax@users.noreply.github.com>
2021-11-19 23:18:04 +0100

    Update tabella.c

--- tabella.c
+++ tabella.c
@@ -5,3 +5,22 @@ int main(void)
    printf("Tabella\n\"\t\'\t\\\n\"\t\'\ta\n");
    return 0;
 }
+
+/*
+   !! modalità diversa per stampare la stessa tabella !!
+
+   {
+     printf("Tabella\n");
+     printf("\"");
+     printf("\t");
+     printf("\'");
+     printf("\t");
+     printf("\\\n");
+     printf("\"");
+     printf("\t");
+     printf("\'");
+     printf("\t");
+     printf("a\n");
+    }
+  
+*/
