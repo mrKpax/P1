1a2f882a026ef5b7d55af8cbb0c2617c8026aab9
mrKpax <94631872+mrKpax@users.noreply.github.com>
2021-11-21 16:47:32 +0100

    Create bisestili.c

---
+++ bisestili.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int bisestile(int anno)
+{
+	if (anno%400==0 || (anno%4==0 && anno%100!=0))
+			{
+				return 1;
+			}
+	else
+			{
+				return 0;
+			}
+}
+
+int main(void)
+{
+	int anno=1600;
+	while (anno<=10000)
+				{
+					if (bisestile(anno))
+							{
+								printf("%d ", anno);
+							}
+					anno= anno+4;
+				}
+	printf("\n");
+}
