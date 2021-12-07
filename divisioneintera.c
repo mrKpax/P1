3719f160c7e23ac2c72a04dd0556ac379fb9df0a
Francesco Passiatore <94631872+mrKpax@users.noreply.github.com>
2021-12-07 19:23:59 +0100

    Update divisioneintera.c

--- divisioneintera.c
+++ divisioneintera.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
-
-void divisioneintera(int dividendo, int divisore, int *quoziente, int *resto)
+void divisioneintera(int dividendo, int divisore, int *ptr_quoziente, int *ptr_resto) 
 {
-  *quoziente = dividendo / divisore;
-  *resto = dividendo % divisore;  
+	*ptr_quoziente = dividendo/divisore;
+	*ptr_resto = dividendo%divisore;
 }
 
 int main(void)
 {
-  int quoziente, resto, dividendo, divisore;
+	int quoziente, resto, dividendo, divisore;
 
-  printf("Inserisci il numero da dividere: ");
-  scanf("%d", &dividendo);
+	printf("inserisci il numero da dividere: ");
+	scanf("%d", &dividendo);
+	printf("inserisci il divisore: ");
+	scanf("%d", &divisore);
 
-  printf("Inserisci il divisore: ");
-  scanf("%d", &divisore);
-
-  divisioneintera(dividendo, divisore, &quoziente, &resto);
-  printf("%d diviso %d fa %d con resto %d", dividendo, divisore, quoziente, resto);
-
-  return 0;  
+	divisioneintera(dividendo, divisore, &quoziente, &resto);
+	printf("%d diviso %d fa %d con resto %d\n", dividendo, divisore, quoziente, resto);
+	return 0;
 }
