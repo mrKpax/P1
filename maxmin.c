f6fd8335cc3fc81476fa7c93a34a0fbf35a32623
Francesco Passiatore <94631872+mrKpax@users.noreply.github.com>
2021-12-07 19:52:11 +0100

    Update maxmin.c

--- maxmin.c
+++ maxmin.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #define N 10
 
-void maxmin(int a[], int n, int *max, int *min)
+void maxmin(int b[], int n, int *ptr_massimo, int *ptr_minimo)
 {
-  *max= *min = a[0];
+	*ptr_massimo = *ptr_minimo = b[0];
 
-  for (int i=1; i<=n-1; i++)
-      {
-        if (a[i] > *max)
-           {
-             *max = a[i];  
-           }  
-        else if (a[i] < *min)
-                {
-                  *min = a[i];  
-                }
-      }  
+	for (int i=0; i<=n-1; i++)
+	{
+		if (b[i]>*ptr_massimo)
+		{
+			*ptr_massimo = b[i];
+		}
+		else if (b[i]<*ptr_minimo)
+	    {
+			*ptr_minimo = b[i];
+	    }
+	}
+
 }
 
 int main(void)
 {
-  int b[N], massimo, minimo;
+  int b[N], i, massimo, minimo;
 
   printf("Inserisci %d numeri: \n", N);
+  for (i=0; i<=N-1; i++)
+  {
+	  scanf("%d", &b[i]);  
+  }	
 
-  for(int i=0; i<=N-1; i++)
-     {
-       scanf("%d", &b[i]);  
-     }
+  maxmin(b, N, &massimo, &minimo);
 
-   maxmin(b, N, &massimo, &minimo);
-   printf("massimo: %d\n", massimo);
-   printf("minimo: %d\n", minimo);
+  printf("massimo: %d\n", massimo);
+  printf("minimo: %d\n", minimo);
 
-   return 0;
+  return 0;
 }
