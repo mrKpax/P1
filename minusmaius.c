b5e5a0f8b7f6536064858c9506b624e9a1e3eafc
Francesco Passiatore <94631872+mrKpax@users.noreply.github.com>
2021-12-15 22:51:26 +0100

    Create minusmaius.c

---
+++ minusmaius.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#define STR_LEN 50
+
+int leggi(char *s)
+{
+    printf("Inserisci la stringa: ");
+    fgets(s, STR_LEN+1, stdin);
+    int len = strlen(s);
+    
+    if (len<STR_LEN+1)
+    {
+        s[len-1] = '\0';
+    }
+    
+    return len;
+}
+
+void minus_maius(char s[], int len)
+{
+    int offset = 'a' - 'A';
+    
+    for (int i=0; i<=len-1; i++)
+    {
+        if(s[i] >= 'A' && s[i] <= 'Z')
+        {
+            s[i] += offset;
+        }
+        else if (s[i] >= 'a' && s[i] <= 'z')
+             {
+                s[i] -= offset;
+             }
+    }
+}
+
+int main(void)
+{
+    int len;
+    char s[STR_LEN+1];
+    
+    len=leggi(s);
+    
+    minus_maius(s, len);
+    
+    printf("La stringa modificata è %s\n", s);
+    
+    return 0;
+    
+}
