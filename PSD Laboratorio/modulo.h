8e0b990e7ded967e2eeec75c1573d954fdfbb88e
Francesco Passiatore <94631872+mrKpax@users.noreply.github.com>
2022-03-15 19:23:17 +0100

    Create modulo.h

---
+++ PSD Laboratorio/modulo.h
@@ -0,0 +1,7 @@
+//file header con funzioni utili
+#include <stdio.h>
+#include <stdlib.h>
+
+void *xmalloc(size_t nbytes);
+void *xcalloc(size_t nelements, size_t elementsize);
+void *xrealloc(void *pointer, size_t size);
